e7e6b66e4ca228f6d262cc07f96f65cc4a08da8a
Muzdalifa <a.muzdalifa27@gmail.com>
2020-09-09 19:47:44 +0200

    Reading and writing data to the UI

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -18,9 +18,17 @@ namespace RazorPagesExample1.Pages
         }
 
         public string FirstName { get; set; }
+
+        [BindProperty(SupportsGet = true)] //you have to pass this value in url like ?LastName=Ali
+        public string LastName { get; set; }
+
         public void OnGet()
         {
+            //writing the name to the UI
             FirstName = "Muzdalifa";
+
+            if (string.IsNullOrEmpty(LastName))
+                LastName = "User";
         }
     }
 }
